24972d6792a8c181f81dd71a62b6267ec4c4b7ce
Damilare Faluyi <92920276+damilareGit@users.noreply.github.com>
2025-05-19 23:00:40 +0100

    Update Assessment_Q3.sql

--- Assessment_Q3.sql
+++ Assessment_Q3.sql
@@ -9,13 +9,13 @@ with transactions as (
 			when pp.is_fixed_investment = 1 then 'Investment'
 			when pp.is_regular_savings = 1 then 'Savings'
 			end as type,
-        max(date(sa.transaction_date)) as last_transaction_date,
-		datediff(curdate(), date(sa.transaction_date)) as inactivity_days
+        max(date(sa.transaction_date)) as last_transaction_date, -- To pick the highest date == the most recent date/day.
+		datediff(curdate(), date(sa.transaction_date)) as inactivity_days -- Extracting date difference between today and the last_transaction_date. 
 	from plans_plan pp
 	join savings_savingsaccount sa
 		on pp.id = sa.plan_id
 	where 
-		pp.is_regular_savings = 1
+		pp.is_regular_savings = 1 -- This filter was required because all data under is_fixed_investment column were NULL values.
 		AND sa.transaction_date IS NOT NULL
 	group by 1,2,3,5)
     
